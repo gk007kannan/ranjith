7cfedfc02c97d42114313ed4ba752478ed5187a6
gk007kannan <35525377+gk007kannan@users.noreply.github.com>
2018-03-19 15:46:01 +0530

    Create dfhgk.c

---
+++ dfhgk.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+    voide main() 
+    {
+    int a,i,x;
+    char b[10];
+    char ch;
+    clrscr();
+    for(i=0;i<10;i++)
+      scanf("%c",&b[i]);
+
+    for(i=0;i<10;i++)
+       if(b[i]!='e')
+           printf("%c",b[i]); 
+getch();
+}
